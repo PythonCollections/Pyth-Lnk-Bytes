46fd88bd8b7ee0a96df4a23c0af9d3ef84090013
Praveen George Yagappa <38196425+yppgeorge@users.noreply.github.com>
2021-01-10 12:32:29 -0700

    Working on learning again added my version and the solution code

--- 00PrimeNumbers.py
+++ 00PrimeNumbers.py
@@ -1,16 +1,36 @@
 
-def getprimefactors(integer):
-    a = list()
-    divisor = 2
-    integer = int(integer)
-    while(divisor <= integer):
-        if(integer%divisor) == 0:
-            a.append(divisor)
-            integer = integer / divisor
+
+def G_get_prime_number(a):
+    print("Given Input is {0}".format(a))
+    primenumbers = [2,3,5,7]
+    factors = []
+    for i in primenumbers:
+        print("Current Operator is {}".format(i))
+        print("Result of the Operator is {}".format(a % i))
+        if(a % i == 0):
+            print("Entered the Loop")
+            factors.append(i)
+            a = a / i
         else:
-            divisor+= 1
-    print(a)
+            print("Skipped the Loop")
+
+    print(factors,primenumbers)
+
+def get_prime_number(a):
+    factors = []
+    divisor = 2
+    while(divisor <= a):
+        if(a % divisor) == 0:
+            factors.append(divisor)
+            a = a / divisor
+        else:
+            divisor += 1
+    print("Factors from solution are {}".format(factors))
+    return factors
+
+
+
 
 if __name__ == "__main__":
-    integer = input("Enter the number")
-    getprimefactors(integer)
+    get_prime_number(630)
+    # G_get_prime_number(630)
